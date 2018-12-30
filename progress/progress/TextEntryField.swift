ecc58c40b47ba10f1e0486bec7af1a4784cb0caa
Thomas Northall-Little <tomnlittle@outlook.com>
2018-12-30 13:46:53 +1100

    custom textfield

---
+++ progress/progress/TextEntryField.swift
@@ -0,0 +1,59 @@
+//
+//  TextEntryField.swift
+//  progress
+//
+//  Created by Thomas Northall-Little on 30/12/18.
+//  Copyright © 2018 Thomas Northall-Little. All rights reserved.
+//
+
+import UIKit
+
+class TextEntryField: UITextField {
+    
+    @IBInspectable var colour: UIColor = UIColor.clear {
+        didSet {
+            setup()
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            setup()
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0.0 {
+        didSet {
+            setup()
+        }
+    }
+    
+    let heightPadding: CGFloat = 10.0
+    
+    //MARK: Initialisation
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    //MARK: Private Methods
+    private func setup() {
+        self.layer.cornerRadius = cornerRadius
+        
+        self.layer.borderColor = colour.cgColor
+        self.layer.borderWidth = borderWidth
+        
+        self.backgroundColor = colour
+        
+        let height = self.font!.pointSize + self.borderWidth + heightPadding
+        NSLayoutConstraint(item: self, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: height).isActive = true
+    
+        // add padding to the left side of the text box
+        let amount: CGFloat = 10
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    
+    }
+
+}
